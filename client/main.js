91eec26bed3e6599aa735293b4dc331b475d8e69
Rhys Powell <rhys@rpowell.me>
2013-06-01 16:01:32 +1000

    More instructions

--- client/main.js
+++ client/main.js
@@ -1,1 +1,3 @@
+// Note you'll need to replace this publishable key with your own and set the
+// PIN_API_SECRET environment variable to your private key.
 Pin.setPublishableKey('pk__3XyFM-1X3EEYMwpZuRzzg');
