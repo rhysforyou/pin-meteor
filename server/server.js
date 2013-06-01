2998683bda0b41bc4e19641551415134da083873
Rhys Powell <rhys@rpowell.me>
2013-06-01 14:53:42 +1000

    Payments now work

---
+++ server/server.js
@@ -0,0 +1,26 @@
+Meteor.methods({
+  createCharge: function(response) {
+    console.log(response)
+
+    if (Meteor.isServer) {
+      data = {
+        email: "fake@example.com",
+        description: "Meteor Test",
+        amount: 500,
+        currency: "AUD",
+        ip_address: "127.0.0.1",
+        card_token: response.response.token
+      }
+
+      console.log(data)
+
+      Meteor.http.post("https://test-api.pin.net.au/1/charges", {
+        data: data,
+        auth: process.env.PIN_API_SECRET + ":"
+      }, function(error, response) {
+        console.log("Got Response")
+        console.log(response)
+      })
+    }
+  }
+})
